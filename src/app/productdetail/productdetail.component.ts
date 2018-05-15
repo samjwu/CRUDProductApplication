81cdded4ca671f7c3c0ce9d558e21c40bda7b419
unknown <swm1000@hotmail.com>
2018-05-14 20:45:44 -0400

    pass product property as input to product detail component

--- src/app/productdetail/productdetail.component.ts
+++ src/app/productdetail/productdetail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from "../product";
 
 @Component({
   selector: 'app-productdetail',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productdetail.component.css']
 })
 export class ProductdetailComponent implements OnInit {
+  @Input() product: Product;
 
   constructor() { }
 

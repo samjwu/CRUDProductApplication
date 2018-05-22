bea23f21d96c2a9215cfcfef78e9539583d0e99c
unknown <swm1000@hotmail.com>
2018-05-21 21:17:16 -0400

    import in memory web api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
@@ -17,7 +21,11 @@ import { ApproutingModule } from './/approuting.module';
   imports: [
     BrowserModule,
     FormsModule,
-    ApproutingModule
+    ApproutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
   ],
   providers: [ ProductService ],
   bootstrap: [AppComponent]
